2140385c919f7dbc8ea523df2137048a0dd3308f
Vincent <vcano.fr@gmail.com>
2024-02-03 15:59:38 +0100

    add gsap and animate header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,24 @@
+"use client";
 import Image from "next/image";
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export default function Header() {
+  const ref = useRef<HTMLDivElement>(null);
+  useGSAP(() => {
+    gsap.set(ref.current, { visibility: "visible" });
+    gsap.from(ref.current!.children, {
+      opacity: 0,
+      duration: 1,
+      stagger: 0.25,
+      ease: "linear",
+      delay: 0.5,
+    });
+  });
   return (
     <header className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="flex flex-col max-w-[22.5rem]">
+      <div className="flex flex-col max-w-[22.5rem] invisible" ref={ref}>
         <div className="relative h-20">
           <Image src="/FIFA.svg" alt="FIFA logo" fill />
         </div>
