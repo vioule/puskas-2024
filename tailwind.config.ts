fa08709e96f2e0f7ada7c490a6a318094ab05818
Vincent <vcano.fr@gmail.com>
2024-02-03 15:19:15 +0100

    set fonts and media querys

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 const config: Config = {
   content: [
@@ -7,11 +8,19 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      ...colors,
+      gold: "#FFC634",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        roboto: ["var(--font-roboto)"],
+        rocksalt: ["var(--font-rocksalt)"],
       },
     },
   },
