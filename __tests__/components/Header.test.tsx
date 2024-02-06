cd6652ca0319e514257c4b36e0137f4324bed4b8
Vincent <vcano.fr@gmail.com>
2024-02-06 10:37:38 +0000

    Modify Header with framer motion

--- __tests__/components/Header.test.tsx
+++ __tests__/components/Header.test.tsx
@@ -9,18 +9,19 @@ test("render Header unchanged", () => {
 test("Header is visible after GSAP animation", async () => {
   render(<Header />);
   const container = screen.getByTestId("container");
-  expect(container).toHaveStyle("visibility: visible");
+  expect(container).toHaveStyle("opacity: 0");
   expect(container.children[0]).toHaveStyle("opacity: 0");
   expect(container.children[1]).toHaveStyle("opacity: 0");
   expect(container.children[2]).toHaveStyle("opacity: 0");
   expect(container.children[2]).toHaveStyle("opacity: 0");
   await waitFor(
     () => {
+      expect(container).toHaveStyle("opacity: 1");
       expect(container.children[0]).toHaveStyle("opacity: 1");
       expect(container.children[1]).toHaveStyle("opacity: 1");
       expect(container.children[2]).toHaveStyle("opacity: 1");
       expect(container.children[2]).toHaveStyle("opacity: 1");
     },
-    { timeout: 2000 }
+    { timeout: 2500 }
   );
 });
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,41 +1,50 @@
 "use client";
 import Image from "next/image";
-import { useRef } from "react";
-import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
+import { motion } from "framer-motion";
 
 export default function Header() {
-  const ref = useRef<HTMLDivElement>(null);
-  useGSAP(() => {
-    gsap.set(ref.current, { visibility: "visible" });
-    gsap.from(ref.current!.children, {
-      opacity: 0,
-      duration: 1,
-      stagger: 0.25,
-      ease: "linear",
-      delay: 0.5,
-    });
-  });
+  const variants = {
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.25,
+        delayChildren: 1,
+        duration: 1,
+      },
+    },
+    hidden: { opacity: 0 },
+  };
   return (
     <header className="flex min-h-screen flex-col items-center justify-center p-24 relative">
-      <div
-        className="flex flex-col max-w-[22.5rem] invisible"
-        ref={ref}
+      <motion.div
+        className="flex flex-col max-w-[22.5rem]"
         data-testid="container"
+        animate="visible"
+        initial="hidden"
+        variants={variants}
       >
-        <div className="relative h-20">
+        <motion.div className="relative h-20" variants={variants}>
           <Image src="/FIFA.svg" alt="FIFA logo" fill />
-        </div>
-        <span className="text-6xl text-gold tracking-[1.5rem] font-black font-roboto mt-4">
+        </motion.div>
+        <motion.span
+          className="text-6xl text-gold tracking-[1.5rem] font-black font-roboto mt-4"
+          variants={variants}
+        >
           PUSKÀS
-        </span>
-        <span className="text-white text-2xl tracking-[0.5rem] text-right">
+        </motion.span>
+        <motion.span
+          className="text-white text-2xl tracking-[0.5rem] text-right"
+          variants={variants}
+        >
           AWARD
-        </span>
-        <div className="flex justify-center items-center gap-1 mt-10">
+        </motion.span>
+        <motion.div
+          className="flex justify-center items-center gap-1 mt-10"
+          variants={variants}
+        >
           <div className="w-[1px] h-4 bg-white animate-scroll origin-top"></div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </header>
   );
 }
